6d40bfe2250be5b930de97fbf1a6aacda279314d
Paul Le Couteur <paullecouteur@gmail.com>
2020-05-02 22:23:58 +0200

    add _document.tsx

---
+++ client/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
+class MainDocument extends Document {
+  static async getInitialProps(context: DocumentContext) {
+    const initialProps = await Document.getInitialProps(context)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MainDocument
