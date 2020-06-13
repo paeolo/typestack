176294ae5535c30343e8b52786e2ad11fecd8ab1
Paul Le Couteur <paullecouteur@gmail.com>
2020-06-13 16:00:31 +0200

    add navbar (wip on client page)

---
+++ client/src/components/Navbar/WithNavBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Navbar.module.scss';
+import { Navbar } from './NavBar';
+
+export const WithNavbar: React.FC = props => {
+  return (
+    <div className={styles.paddingTop}>
+      <Navbar />
+      {props.children}
+    </div>
+  )
+}
--- client/src/pages/[lang]/index.tsx
+++ client/src/pages/[lang]/index.tsx
@@ -1,12 +1,20 @@
+import React from "react";
 import { GetServerSideProps } from "next";
 
 import { IndexPage } from "@components/Pages";
 import { WithLocale, getLocaleProps } from "@components/I18n";
 import { WithAuthentication, getAuthenticationProps } from "@components/Authentication";
+import { HtmlHead } from "@components/Core";
+import { WithNavbar } from "@components/Navbar";
 
 const Index = () => {
   return (
-    <IndexPage />
+    <React.Fragment>
+      <HtmlHead />
+      <WithNavbar>
+        <IndexPage />
+      </WithNavbar>
+    </React.Fragment>
   );
 }
 
