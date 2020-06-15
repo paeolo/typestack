d03b966208dc1a81a3ba66f135d110a46af192a1
Paul Le Couteur <paullecouteur@gmail.com>
2020-06-16 00:20:02 +0200

    wip

---
+++ client/src/pages/[lang]/register.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { GetServerSideProps } from "next";
+
+import { IndexPage } from "@components/Pages";
+import { WithLocale, getLocaleProps } from "@components/I18n";
+import { WithAuthentication, getAuthenticationProps } from "@components/Authentication";
+import { HtmlHead } from "@components/Core";
+import { WithNavbar } from "@components/Navbar";
+
+const Index = () => {
+  return (
+    <React.Fragment>
+      <HtmlHead />
+      <WithNavbar>
+        <IndexPage />
+      </WithNavbar>
+    </React.Fragment>
+  );
+}
+
+export default WithLocale(WithAuthentication(Index));
+
+export const getServerSideProps: GetServerSideProps = async context => {
+
+  const lang = context.params.lang as string;
+  const localeProps = await getLocaleProps(lang);
+  const authenticationProps = await getAuthenticationProps(context.req);
+  return {
+    props: {
+      ...authenticationProps,
+      ...localeProps
+    }
+  }
+}
